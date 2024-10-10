4c37e80bd3c534fce6f5cff1aea3808403d6b9f2
Arun2089 <arunlohar@gkmit.co>
2024-10-10 17:59:15 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,5 +3,7 @@ output=$(aws secretsmanager get-secret-value --secret-id envs)
 secret_string=$(echo "$output" | jq -r '.SecretString')
 key_value=$(echo "$secret_string" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"')
 echo "$key_value" > arun.env
+
+rm -f arun.env.cpt
 ccrypt -e -k arun arun.env
 rm -f arun.env
