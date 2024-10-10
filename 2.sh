3b7e108030129c4a6e166fb4766832bd8b2f6aa9
Arun2089 <arunlohar@gkmit.co>
2024-10-10 17:15:59 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -2,3 +2,6 @@ ccrypt -d -k arun arun.env.cpt
 docker-compose up -d 
 ccrypt -e -k arun arun.env
 rm -f arun 
+
+
+docker exec  my_nginx /bin/bash -c "cd /usr/share/nginx/html/ && echo '${name}' > index.html"
